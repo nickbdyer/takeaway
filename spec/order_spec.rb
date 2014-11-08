597b21c07d6efde2b2d15248c0467e2f7a122ba8
Nick Dyer <nbdyer@gmail.com>
2014-11-08 20:40:55 +0000

    Order can show its contents

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,6 +4,7 @@ describe "Order" do
 
   let(:order) { Order.new }
   let(:ham_and_cheese_toastie) { double :dish }
+  let(:penne_arrabbiata) { double :dish }
 
   it "should allow dishes to be added" do
     order.add(ham_and_cheese_toastie)
@@ -14,6 +15,17 @@ describe "Order" do
     order.add(ham_and_cheese_toastie)
     order.remove(ham_and_cheese_toastie)
     expect(order.contents).to eq []
+  end
+
+  it "should be possible to show the current order" do
+    order.add(penne_arrabbiata)
+    order.add(ham_and_cheese_toastie)
+    expect(order.contents).to eq [penne_arrabbiata, ham_and_cheese_toastie]
+  end
+
+  it "should calculate the total of the order" do
+
+
   end
 
 end
