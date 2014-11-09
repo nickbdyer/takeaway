c51bde66fe8fa099bde63072aa515880bd95a5c7
Nick Dyer <nbdyer@gmail.com>
2014-11-09 09:43:34 +0000

    Takeaway can create an order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,13 +1,19 @@
 class Takeaway
 
   attr_reader :menu
+  attr_reader :orders
 
   def initialize
     @menu = nil
+    @orders = []
   end
 
   def add_menu(menu)
     @menu = menu
+  end
+
+  def create_new(content, customer)
+    orders << content.new(customer)
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,8 +2,12 @@ require 'takeaway'
 
 describe "Takeaway" do
 
-  let(:the_chutney) { Takeaway.new }
-  let(:dinner_menu) { double :menu }
+  let(:the_chutney) { Takeaway.new                       }
+  let(:dinner_menu) { double :menu                       }
+  let(:order_class) { double :order_class, :new => order }
+  let(:rich)        { double :customer                   }
+  let(:order)       { double :order                      }
+
 
   it "should not have a menu when initialized" do 
     expect(the_chutney.menu).to be_falsy
@@ -13,7 +17,11 @@ describe "Takeaway" do
     the_chutney.add_menu(dinner_menu)
     expect(the_chutney.menu).to eq dinner_menu
   end
-
+ 
+  it "should allow an order to be created" do
+    the_chutney.create_new(order_class, rich)
+    expect(the_chutney.orders).to eq [order]
+  end
 
 
 end
